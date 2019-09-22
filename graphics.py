fbe6a86686539e027da153d4b9e3decdf91f7190
Juan Esteban Cepeda <Juanchobanano>
2019-09-21 22:51:00 -0500

    Nuevos Cambios

--- graphics.py
+++ graphics.py
@@ -12,7 +12,6 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("acc_data.csv", delimiter = ",")
 df.columns = "category Xmax Xmin Xstd Ymax Ymin Ystd Zmax Zmin Zstd Tmax Tmin Tstd".split()
 
-
 sns.set(rc={'figure.figsize':(8,4)})
 ax2 = sns.scatterplot(x="Xmax", y="Tmax",  hue="category", style="category", s=100, data=df)
 ax = plt.gca()
--- trainingAlgo2.py
+++ trainingAlgo2.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Wed Sep 11 09:41:48 2019
+
+@author: juanc
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Editor de Spyder
 Este es un archivo temporal.
 """
@@ -7,10 +14,7 @@ Este es un archivo temporal.
 import socket
 import matplotlib.pyplot as plt
 import time
-import auxiliar as axfunc
 import csv
-import numpy as np
-import naive_bayes
 
 # Graphing helper function
 def setup_graph(title='', x_label='', y_label='', fig_size=None):
@@ -22,9 +26,9 @@ def setup_graph(title='', x_label='', y_label='', fig_size=None):
     ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
 
-UDP_IP = "10.203.162.231" #"192.168.43.156" #0.14
+UDP_IP = "192.168.0.8" #"192.168.43.156" #0.14
 UDP_PORT = 5555
-CATEGORY = 1
+CATEGORY = 0
   #-1 => Testing, 0 => Still, 1 => Moving, 2 => Fall
 MODE = "TRAINING" #"TRAINING" # "TESTING"
 DECIMALS = 3
@@ -43,7 +47,7 @@ for i in range(0, 4):
     acelerometro.append(list())
     
 contador = 0    
-FRAMES = 15
+FRAMES = 60
 
 start_time = time.time()
     
@@ -71,64 +75,24 @@ while contador < FRAMES:
     
     contador += 1
     
+    
+print(contador)
 # Get features from data.
 categoria = CATEGORY
-data = axfunc.getFeatures(categoria, acelerometro, decimals = DECIMALS)
-print(data)
+#data = axfunc.getFeatures(categoria, acelerometro, decimals = DECIMALS)
+#print(data)
 
 # Print elapsed time.
 elapsed_time = time.time() - start_time
 print("Elapsed Time: ", elapsed_time)
 
-# Training Mode.
-if MODE == "TRAINING":
-          
-    # Save in CSV.
-    if(input("Include in DataSet? YES/NO: ") == "Y"):
-        with open("./acc_data.csv", "a") as writeFile:
-             writer = csv.writer(writeFile)
-             writer.writerow(data)
-        writeFile.close()
-        print("Included!")
-    else: 
-        print("Not included")
-
-# Testing Mode.
-elif MODE == "TESTING":
-    classifier = naive_bayes.NaiveBayesClassifier("acc_data.csv")
-    print("Classification: ", classifier.classifyEntry(data))
-
-    
+        
 # Plot X, Y, Z
 x = list()
 for i in range(0, FRAMES):
     x.append(i+1)
     
-    
-# 
-time_to_plot = 2 # second
-sample_rate = 100 # samples per second
-num_samples = sample_rate * time_to_plot
-    
-fft_output = np.fft.rfft(acelerometro[3])
-magnitude_only = [np.sqrt(i.real**2 + i.imag**2)/len(fft_output) for i in fft_output]
-frequencies = [(i*1.0/num_samples)*sample_rate for i in range(num_samples//2+1)]
-
-setup_graph(x_label='frequency (in Hz)', y_label='amplitude', title='frequency domain')
-plt.plot(frequencies, magnitude_only, 'r')
-
-# Plot results.
-
-"""
-plot1, = plt.plot(x, acelerometro[0])
-plot2, = plt.plot(x, acelerometro[1])
-plot3, = plt.plot(x, acelerometro[2])
-plt.legend([plot1, plot2, plot3], ["X", "Y", "Z"])
-plt.ylabel("Values")
-plt.xlabel("Frames")
-plt.title("Accelerometer Info")
-plt.show()
-
+        
 # Plot total.
 plot4, = plt.plot(x, acelerometro[3])
 plt.title("Accelerometer Total")
@@ -136,12 +100,17 @@ plt.legend([plot4], "Total")
 plt.ylabel("Values")
 plt.xlabel("Frames")
 plt.show()
-"""
 
-# Close socket
-sock.close()
-
-
-
-#category,Xmean,Xmax,Xmin,Xstd,Ymean,Ymax,Ymin,Ystd,Zmean,Zmax,Zmin,Zstd
-#category,Xmax,Xstd,Ymax,Ystd,Zmax,Zstd
+# Training Mode.
+if MODE == "TRAINING":
+          
+    # Save in CSV.
+    if(input("Include in DataSet? YES/NO: ") == "Y"):
+        with open("./acc_data4.csv", "a") as writeFile:
+             writer = csv.writer(writeFile)
+             acelerometro[3].insert(0, CATEGORY)
+             writer.writerow(acelerometro[3])
+        writeFile.close()
+        print("Included!")
+    else: 
+        print("Not included")
